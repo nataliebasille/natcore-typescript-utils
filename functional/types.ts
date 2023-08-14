74bcb60d9e877c10200e1070d7a222a00f594afc
Natalie Basille <natalie.basille1@gmail.com>
2023-08-14 16:49:55 -0400

    Add description to CullType

--- functional/types.ts
+++ functional/types.ts
@@ -1,3 +1,10 @@
+/**
+ * Returns a type where the elements of `TTuple` that are in `TPartial`
+ * and in the same order are removed.
+ *
+ * @example type CullTuple<[1, 2, 3, 4], [1, 3]> = [2, 4]
+ * @example type CullTuple<[1, 2, 3, 4], [3, 1]> = [1, 2, 4]
+ */
 export type CullTuple<
   TTuple extends any[],
   TPartial extends any[]
