4989bcc3607862fd46ce6219c2d2c9cf64d3bc21
Natalie Basille <natalie.basille1@gmail.com>
2024-07-29 22:32:52 -0400

    delete unused file

--- functional/result.ts
+++
@@ -1,6 +0,0 @@
-export const isResult = (value: unknown): value is Result<unknown, unknown> =>
-  typeof value === 'object' &&
-  value !== null &&
-  'type' in value &&
-  'value' in value &&
-  (value.type === 'ok' || value.type === 'error');
